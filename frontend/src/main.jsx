4b40f528745299fcf67a05c86d52d43b482cf2f0
rameshthanikonda <rchowdhary3636@gmail.com>
2025-11-01 23:26:59 +0530

    second  commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,17 @@
 // src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import { AuthProvider } from './context/AuthContext'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode> {/* Added StrictMode back for stability */}
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider> 
         <App /> 
       </AuthProvider>
-   </BrowserRouter>
+   </HashRouter>
   </React.StrictMode>
 );
